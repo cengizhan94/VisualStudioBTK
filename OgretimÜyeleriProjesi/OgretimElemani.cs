8c0ca3bec2f3f980cce115e29930b4057b4d1f7e
Cengiz Han <90911173+cengizhan94@users.noreply.github.com>
2021-11-17 16:48:10 +0300

    VisualStudio_Dersleri_BTKAkademi
    OgretimUyeleriProjesi

---
+++ OgretimÜyeleriProjesi/OgretimElemani.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OgretimÜyeleriProjesi
+{
+
+   public class OgretimElemani
+    {      //Property - Özellikleri
+
+        public int SicilNo { get; set; }
+        public string Adi { get; set; }
+        public string Soyadi { get; set; }
+        public bool Cinsiyeti { get; set; }
+
+
+        public OgretimElemani()
+        {
+            
+
+        }
+
+        public OgretimElemani(int sicilNo, string adi, string soyadi, bool cinsiyeti)
+        {
+            SicilNo = sicilNo;
+            Adi = adi;
+            Soyadi = soyadi;
+            Cinsiyeti = cinsiyeti;
+        }
+
+        public override string ToString()
+        {
+            return $" {SicilNo,-5} " +
+                $"{Adi,-10}" +
+                $"{Soyadi, - 10}" +
+               string.Format("{0,-8}", Cinsiyeti == true ? "Bay" : "Bayan");
+             
+
+
+                    
+
+        }
+    }
+         
+         
+
+
+    
+}
---
+++ OgretimÜyeleriProjesi/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OgretimÜyeleriProjesi
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var Liste1 = new List<OgretimElemani>()
+           {
+                new OgretimElemani(100, "Cengiz Han", "Uyar", true),
+                new OgretimElemani(101, "Zeynep", "Uyar", false),
+                new OgretimElemani(102, "Pervin", "Oflaz", false),
+                new OgretimElemani(103, "Mustafa", "Kurnaz", true),
+                new OgretimElemani(104, "Ertuğrul", "Gazi", true),
+                new OgretimElemani(105, "Osman", "Gazi", false),    
+            };
+            Console.WriteLine("Liste 1");
+            Liste1.ForEach(ogr => Console.WriteLine(ogr));
+            Console.WriteLine();
+
+            var Liste2 = Liste1;
+            Console.WriteLine("Liste 2");
+            Liste2.ForEach(ogr => Console.WriteLine(ogr));
+            Console.ReadKey();
+            
+
+        }
+
+
+    }
+}
