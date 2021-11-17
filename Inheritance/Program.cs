6c6bcb3f1180603ba4681bc65ef5a7205312d7d4
Cengiz Han <90911173+cengizhan94@users.noreply.github.com>
2021-11-17 16:51:37 +0300

    inheritance

---
+++ Inheritance/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person[] persons = new Person[3]
+            {
+                new Customer{FirstName = "Cengiz"},
+                new Student {FirstName = "Zeynep"},
+                new Person {FirstName = "Pervin"}
+
+            };
+            foreach (var person in persons)
+            {
+                Console.WriteLine(person.FirstName);
+            }
+
+            Console.ReadLine();
+        }
+
+        class Person
+        {
+            public int Id { get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+        }
+
+        class Student : Person
+        {
+
+        }
+
+        class Customer : Person
+        {
+
+        }
+
+
+
+
+
+
+
+
+
+    }
+}
