954f2a02e2d719a25b8173d6e2531ba017be209e
Cengiz Han <90911173+cengizhan94@users.noreply.github.com>
2021-11-17 16:57:29 +0300

    StudentDemo

---
+++ Ogrennciuygulaması/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ogrennciuygulaması
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool kontrol = true;
+            ogrenci ogrenci1 = new ogrenci(1, "Cengiz Han", "Uyar", 50, 40, 73,  "Hakkari Üniversitesi");
+            Console.WriteLine("Uygulamaya Hoşgeldin. Yapmak İstediğin İşlemi Seç: ");
+            islemlerigoster();
+
+            while (kontrol)
+            {
+
+                string secim = Console.ReadLine();
+
+
+                switch (secim)
+                {
+                    case "1":
+                        ogrenci1.ogrencibilgilerigoster();
+                        break;
+
+                    case "2":
+                        double ogrenciortalama = ogrenci1.ogrencinotubul();
+                        Console.WriteLine("Öğrenci Ortalaması: "+ ogrenciortalama);
+                        break;
+
+                    case "3":
+                        ogrenci1.okulgetir();
+                        break;
+                    case "4":
+                        Console.WriteLine("Çıkış Başarılı");
+                        kontrol = false;
+                        break;
+
+                }
+                
+
+            }
+
+
+        }
+
+        static void islemlerigoster()
+        {
+            Console.WriteLine("1- Öğrenci Bilgilerini Gör: ");
+            Console.WriteLine("2- Öğrenci Ortalamasını Gör: ");
+            Console.WriteLine("3- Öğrenci Okulunu Gör: ");
+            Console.WriteLine("4- Çıkış Yap!");
+
+        }
+    }
+}
---
+++ Ogrennciuygulaması/ogrenci.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ogrennciuygulaması
+{
+    class ogrenci
+    {  //<Tanımlama
+        private int ogrno;
+        private string Adi;
+        private string soyAdi;
+        private int vize1;
+        private int vize2;
+        private int final;
+        private string okuladi;
+
+        public ogrenci(int _ogrno, string _isim, string _soyisim, int _vize1, int _vize2, int _final, string _okul)//<Atama Yaptım
+        {
+            ogrno = _ogrno;
+            Adi = _isim;
+            soyAdi = _soyisim;
+            vize1 = _vize1;
+            vize2 = _vize2;
+            final = _final;
+            okuladi = _okul;
+
+        }
+
+
+
+
+        public void ogrencibilgilerigoster() //<Yazdırma
+        {
+            Console.WriteLine("Öğrenci Numarası: " + ogrno);
+            Console.WriteLine("Öğrencinin Adı: " + Adi);
+            Console.WriteLine("Öğrencinin Soyadı: " + soyAdi);
+            Console.WriteLine("Öğrenci Vize1 Notu" + vize1);
+            Console.WriteLine("Öğrenci Vize2 Notu" + vize2);
+            Console.WriteLine("Öğrenci Final Notu" + final);
+            Console.WriteLine("Öğrenci Okul Adı" + okuladi);
+
+        }
+
+        public double ogrencinotubul() //< Ortalama hesaplama
+        {
+            double ogrenciortalama = (vize1 * 0.2 + vize2 * 0.2 + final * 0.6);
+            
+            return ogrenciortalama;
+        }
+
+        public void okulgetir()
+        {
+            Console.WriteLine("Öğrencinin Okul İsmi: " + okuladi);
+        }
+
+    }
+}
